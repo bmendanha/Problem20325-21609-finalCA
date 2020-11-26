ca7585374cccc2f0650de7afffa40926a0ce5b98
Bruno Mendanha <bmendanha@hotmail.com>
2020-11-26 14:34:12 +0000

    Initial commit 20325-Bruno Mendanha 21609 - Mark

---
+++ app/src/main/java/com/example/a20325_21609_finalca/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a20325_21609_finalca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
